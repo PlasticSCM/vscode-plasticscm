01c1b009345cdf3669cb529dfabf88afdb36b29d
Miguel González <mgonzalez@codicesoftware.com>
2019-12-18 18:30:21 +0100

    Use chai and sinon

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,13 +1,15 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import * as chai from 'chai';
 
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
-    ui: 'tdd',
+    ui: 'bdd',
   });
   mocha.useColors(true);
+  chai.use(require('sinon-chai'));
 
   const testsRoot = path.resolve(__dirname, '..');
 
