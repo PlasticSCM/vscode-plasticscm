427bf32220944dd25c03af261ef94af74fc437b3
Miguel González <mgonzalez@codicesoftware.com>
2019-12-19 12:36:04 +0100

    Complete test

--- src/test/suite/cmShell.test.ts
+++ src/test/suite/cmShell.test.ts
@@ -2,21 +2,27 @@ import { expect } from 'chai';
 import { IMock, Mock, MockBehavior, It, Times } from 'typemoq';
 
 import { CmShell, ICmdParser } from '../../cmShell';
-import { OutputChannel, OnTypeFormattingEditProvider } from 'vscode';
+import { OutputChannel } from 'vscode';
 
 describe('CmShell', () => {
   context('not running', () => {
     const channel: IMock<OutputChannel> = Mock.ofType<OutputChannel>(
       undefined, MockBehavior.Strict);
+    channel.setup(ch => ch.appendLine(It.isAnyString()));
 
     const parser: IMock<ICmdParser<any>> = Mock.ofType<ICmdParser<any>>(
       undefined, MockBehavior.Strict);
 
     const cmShell: CmShell = new CmShell('mydir', channel.object);
 
-    it("shouldn't run commands", () => {
-      cmShell.exec('location', [], parser.object)
+    it("shouldn't run commands", async () => {
+      const result = await cmShell.exec('location', [], parser.object)
       expect(cmShell.isRunning).to.be.false;
+      expect(result).to.be.not.null.and.not.undefined;
+      expect(result.success).to.be.false;
+      expect(result.error).to.be.not.undefined;
+      expect(result.error).to.have.property('message', 'Shell wasn\'t running');
+      expect(result.result).to.be.undefined;
 
       channel.verify(
         ch => ch.appendLine(It.is(
