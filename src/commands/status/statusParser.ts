1ddf6a848f6bbc1b94dc874fdd0e0cb343ec9fec
Miguel González <mgonzalez@codicesoftware.com>
2019-12-26 17:46:40 +0100

    Implement the status parser, first draft
    The actual parsing process is still pending to be done
    
    Co-authored-by: Sergio Luis Para <sluisp@codicesoftware.com>

--- src/commands/status/statusParser.ts
+++ src/commands/status/statusParser.ts
@@ -1,24 +1,50 @@
+import * as os from "os";
+import * as xml2js from "xml2js";
 import { ICmParser } from "../../cmShell";
 import { IChangeInfo } from "../../models";
 
 export class StatusParser implements ICmParser<IChangeInfo[]> {
+  private readonly mOutputBuffer: string[] = [];
+  private readonly mErrorBuffer: string[] = [];
+  private mParseError?: Error;
+
   public readLineOut(line: string): void {
-    throw new Error("Method not implemented.");
+    this.mOutputBuffer.push(line);
   }
 
   public readLineErr(line: string): void {
-    throw new Error("Method not implemented.");
+    this.mErrorBuffer.push(line);
   }
 
   public parse(): IChangeInfo[] | undefined {
-    throw new Error("Method not implemented.");
+    let results: IChangeInfo[] = [];
+
+    xml2js.parseString(this.mOutputBuffer.join(""), (error, result) => {
+      if (error) {
+        this.mParseError = error;
+        return;
+      }
+      results = results.concat(this.parseXml(result));
+    });
+    return results;
   }
 
   public getError(): Error | undefined {
-    throw new Error("Method not implemented.");
+    if (this.mParseError) {
+      return this.mParseError;
+    }
+
+    return this.mErrorBuffer.length !== 0
+      ? new Error(this.mErrorBuffer.join(os.EOL))
+      : undefined;
   }
 
   public getOutputLines(): string[] {
-    throw new Error("Method not implemented.");
+    return this.mOutputBuffer.concat(this.mErrorBuffer);
+  }
+
+  private parseXml(result: any): IChangeInfo[] {
+    // TODO implement the actual parsing process
+    return [];
   }
 }
