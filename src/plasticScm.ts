505ef4c24b641a0066e58e4cae5fd48cfa5cbe4a
Miguel González <mgonzalez@codicesoftware.com>
2019-12-26 17:45:36 +0100

    The PlasticSCM class should start shells
    Every workspace should receive an alive shell in their constructor.
    We also need to stop and dispose the global shell.
    
    Co-authored-by: Sergio Luis Para <sluisp@codicesoftware.com>

--- src/plasticScm.ts
+++ src/plasticScm.ts
@@ -7,7 +7,7 @@ import {
   workspace as VsCodeWorkspace,
 } from "vscode";
 import { CmShell, ICmShell } from "./cmShell";
-import { GetWorkspaceFromPath } from "./commands";
+import { GetWorkspaceFromPath, Status } from "./commands";
 import { IWorkspaceInfo } from "./models";
 import { Workspace } from "./workspace";
 
@@ -25,8 +25,8 @@ export class PlasticScm implements Disposable {
       return;
     }
 
-    const shell: ICmShell = new CmShell(os.tmpdir(), this.mChannel);
-    if (!await shell.start()) {
+    const globalShell: ICmShell = new CmShell(os.tmpdir(), this.mChannel);
+    if (!await globalShell.start()) {
       const errorMessage = 'Plastic SCM extension can\'t start: unable to start "cm shell"';
       VsCodeWindow.showErrorMessage(errorMessage);
       this.mChannel.appendLine(errorMessage);
@@ -35,23 +35,32 @@ export class PlasticScm implements Disposable {
 
     for (const folder of VsCodeWorkspace.workspaceFolders) {
       try {
-        const plasticWorkspace: IWorkspaceInfo | undefined =
-          await GetWorkspaceFromPath.run(folder.uri.fsPath, shell);
+        const wkInfo: IWorkspaceInfo | undefined =
+          await GetWorkspaceFromPath.run(folder.uri.fsPath, globalShell);
 
-        if (!plasticWorkspace || this.mWorkspaces.has(plasticWorkspace.id)) {
+        if (!wkInfo || this.mWorkspaces.has(wkInfo.id)) {
           continue;
         }
 
-        const cmShell: ICmShell = new CmShell(plasticWorkspace.path, this.mChannel);
-        const workspace: Workspace = new Workspace(plasticWorkspace, cmShell);
-        this.mDisposables.push(cmShell, workspace);
+        const wkShell: ICmShell = new CmShell(wkInfo.path, this.mChannel);
+        if (!await wkShell.start()) {
+          this.mChannel.appendLine(`Unable to start shell for workspace "${wkInfo.path}"`);
+          wkShell.dispose();
+          continue;
+        }
 
-        this.mWorkspaces.set(plasticWorkspace.id, workspace);
+        const workspace: Workspace = new Workspace(wkInfo, wkShell);
+        this.mDisposables.push(wkShell, workspace);
+        this.mWorkspaces.set(wkInfo.id, workspace);
 
+        const changes = await Status.run(wkInfo.path, wkShell);
       } catch (error) {
         VsCodeWindow.showErrorMessage(error?.message);
         this.mChannel.appendLine(
           `Unable to find workspace in ${folder.uri.fsPath}: ${error?.message}`);
+      } finally {
+        await globalShell.stop();
+        globalShell.dispose();
       }
     }
   }
