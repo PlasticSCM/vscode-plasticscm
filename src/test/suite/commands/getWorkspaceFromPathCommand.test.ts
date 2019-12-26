b03fc7941bd6c0b4f3642bc0f53c8b537028b27b
Sergio Luis Para <sluisp@codicesoftware.com>
2019-12-26 13:19:22 +0000

    Continue implementing tests
    Co-authored-by: Miguel González <mgonzalez@codicesoftware.com>

--- src/test/suite/commands/getWorkspaceFromPathCommand.test.ts
+++ src/test/suite/commands/getWorkspaceFromPathCommand.test.ts
@@ -1,66 +1,91 @@
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
-import { GetWorkspaceFromPath, GetWorkspaceFromPathResult } from '../../../commands/getWorkspaceFromPath';
-import { CmShellMock } from './cmShellMock';
+import { assert, expect } from "chai";
+import { IMock, It, Mock, MockBehavior, Times } from "typemoq";
+import { ICmParser, ICmShell } from "../../../cmShell";
+import { GetWorkspaceFromPath } from "../../../commands";
+import { IWorkspaceInfo } from "../../../models";
 
-chai.use(chaiAsPromised);
+describe("GetWorkspaceFromPath Command", () => {
+  context("Successfully executes the command", () => {
+    it("receives correct output", async () => {
+      const cmShellMock: IMock<ICmShell> = Mock.ofType<ICmShell>(undefined, MockBehavior.Strict);
+      cmShellMock
+        .setup(mock => mock.exec(
+            It.is(path => path === "/foo/bar"),
+            It.is(args => true),
+            It.is<ICmParser<IWorkspaceInfo | undefined>>(parser => true)))
+        .returns(() => Promise.resolve({
+          result: {
+            id: "95b0a429-7d9c-48af-8b5b-6f1ced257b20",
+            name: "wkname",
+            path: "/path/to/wk",
+          },
+          success: true,
+        }));
 
-describe('GetWorkspaceFromPathCommand', () => {
-  context('command successfully executed', () => {
-    it("parses a well-formed output", async () => {
-      const outputLines: string[] = ['wkname\t/path/to/wk\t95b0a429-7d9c-48af-8b5b-6f1ced257b20'];
-      const cmShellMock: CmShellMock = new CmShellMock(outputLines, [], true);
+      const cmdResult: IWorkspaceInfo | undefined =
+        await GetWorkspaceFromPath.run("/foo/bar", cmShellMock.object);
 
-      const cmdResult: GetWorkspaceFromPathResult | undefined =
-        await GetWorkspaceFromPath.run('/foo/bar', cmShellMock);
+      expect(cmdResult).to.be.not.undefined;
+      expect(cmdResult!.path).to.be.equal("/path/to/wk");
+      expect(cmdResult!.name).to.be.equal("wkname");
+      expect(cmdResult!.id).to.be.string("95b0a429-7d9c-48af-8b5b-6f1ced257b20");
 
-      chai.expect(cmdResult).to.not.be.undefined;
-
-      if (!cmdResult)
-        return;
-
-      chai.expect(cmdResult.path).to.be.string(
-        '/path/to/wk', 'Wrong workspace path');
-      chai.expect(cmdResult.name).to.be.string(
-        'wkname', 'Wrong workspace name');
-      chai.expect(cmdResult.id).to.be.string(
-        '95b0a429-7d9c-48af-8b5b-6f1ced257b20', 'Wrong workspace id');
+      cmShellMock.verify(
+        mock => mock.exec(It.isAny(), It.isAny(), It.isAny()),
+        Times.once());
     });
 
-    it("parses a non-well-formed output", async () => {
-      const outputLines: string[] = ['wkname\t/path/to/wk'];
-      const cmShellMock: CmShellMock = new CmShellMock(outputLines, [], true);
+    it("receives incorrect output", async () => {
+      const cmShellMock = Mock.ofType<ICmShell>(undefined, MockBehavior.Strict);
+      cmShellMock
+      .setup(mock => mock.exec(
+          It.is(path => path === "/foo/bar"),
+          It.is(args => true),
+          It.is<ICmParser<IWorkspaceInfo | undefined>>(parser => true)))
+        .returns(() => Promise.resolve({
+          error: new Error("Sample error"),
+          success: true,
+        }));
 
-      const cmdResult: GetWorkspaceFromPathResult | undefined =
-        await GetWorkspaceFromPath.run('/foo/bar', cmShellMock);
+      try {
+        const result: IWorkspaceInfo | undefined = await GetWorkspaceFromPath.run(
+          "/foo/bar", cmShellMock.object);
+        assert.fail("Command should have thrown an error");
+      } catch (error) {
+        expect(error).to.be.not.undefined.and.property("error", "Sample error");
+      }
 
-      chai.expect(cmdResult).to.be.undefined;
+      cmShellMock.verify(
+        mock => mock.exec(It.isAny(), It.isAny(), It.isAny()),
+        Times.once());
     });
   });
 
-  context('error occured', () => {
-    it("displays error from output stream", async () => {
-      const errorMessage = '/foo/bar is not in a workspace.';
-      const outputLines: string[] = [errorMessage];
-      const cmShellMock: CmShellMock = new CmShellMock(outputLines, [], false);
+  context("An error occured", () => {
+    it("receives an unsuccessful result", async () => {
+      const cmShellMock = Mock.ofType<ICmShell>(undefined, MockBehavior.Strict);
+      cmShellMock
+        .setup(mock => mock.exec(
+          It.is(path => path === "/foo/bar"),
+          It.is(args => true),
+          It.is<ICmParser<IWorkspaceInfo>>(parser => true)))
+        .returns(() => Promise.resolve({
+          error: new Error("Sample error"),
+          success: true,
+        }));
 
-      const result : Promise<GetWorkspaceFromPathResult | undefined> =
-        GetWorkspaceFromPath.run('/foo/bar', cmShellMock);
+      try {
+        const result: IWorkspaceInfo | undefined = await GetWorkspaceFromPath.run(
+          "/foo/bar", cmShellMock.object);
+        assert.fail("Command should have thrown an error");
+      } catch (error) {
+        expect(error).to.be.not.undefined.and.property(
+          "error", "Command failed: Sample error");
+      }
 
-      await chai.expect(result).to.be.rejectedWith(
-        errorMessage, 'Wrong error message');
-    });
-
-    it("displays error from error stream", async () => {
-      const errorMessage = 'Unexpected error notified through the error stream.';
-      const errorLines: string[] = [errorMessage];
-      const cmShellMock: CmShellMock = new CmShellMock([], errorLines, false);
-
-      const result : Promise<GetWorkspaceFromPathResult | undefined> =
-        GetWorkspaceFromPath.run('/foo/bar', cmShellMock);
-
-      await chai.expect(result).to.be.rejectedWith(
-        errorMessage, 'Wrong error message');
+      cmShellMock.verify(
+        mock => mock.exec(It.isAny(), It.isAny(), It.isAny()),
+        Times.once());
     });
   });
 });
