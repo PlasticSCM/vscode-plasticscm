78f97026f148aa620c48dac65a98b3a25f2c7d0a
Miguel González <mgonzalez@codicesoftware.com>
2019-12-26 17:44:06 +0100

    Fully implemented the status command
    Co-authored-by: Sergio Luis Para <sluisp@codicesoftware.com>

--- src/commands/status/status.ts
+++ src/commands/status/status.ts
@@ -1,11 +1,24 @@
-import { ICmParser } from "../../cmShell";
+import { ICmParser, ICmResult, ICmShell } from "../../cmShell";
 import { IChangeInfo } from "../../models";
 import { StatusParser } from "./statusParser";
 
 export class Status {
-  public static async run(path: string): Promise<IChangeInfo[]> {
+  public static async run(rootDir: string, shell: ICmShell): Promise<IChangeInfo[]> {
     const parser: ICmParser<IChangeInfo[]> = new StatusParser();
 
-    throw new Error("Not implemented exception");
+    const result: ICmResult<IChangeInfo[]> = await shell.exec(
+      "status",
+      [rootDir, "--xml", "--encoding=utf-8", "--fp"],
+      parser);
+
+    if (!result.success) {
+      throw new Error("Command execution failed.");
+    }
+
+    if (result.error) {
+      throw result.error;
+    }
+
+    return result.result ?? [];
   }
 }
