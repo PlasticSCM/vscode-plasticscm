7fa8862d309d16cc04d60ba5c04dde73735396fd
Sergio Luis Para <sluisp@codicesoftware.com>
2019-12-27 12:29:58 +0100

    Remove local tslint rule disablement

--- src/models/changeInfo.ts
+++ src/models/changeInfo.ts
@@ -8,7 +8,6 @@ export enum WkConfigType {
   Unknown,
 }
 
-// tslint:disable: no-bitwise
 export enum ChangeType {
   Controlled = 0,
   Private    = 1 << 0,
@@ -18,7 +17,6 @@ export enum ChangeType {
   Moved      = 1 << 4,
   Deleted    = 1 << 5,
 }
-// tslint:enable: no-bitwise
 
 export interface IPendingChanges {
   readonly workspaceConfig: IWorkspaceConfig;
