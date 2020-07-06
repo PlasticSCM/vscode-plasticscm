b8269f0f25a0e718cece043ec7098a39d230462d
Miguel González <mgonzalez@codicesoftware.com>
2020-07-06 15:22:54 +0200

    Use modules instead of classes for commands
    It makes more sense to have modules instead of classes if we intend a
    typescript class to work as a static C# class.  I'll edit the checkin
    command in the next commit.
    
    Signed-off-by: Miguel González <mgonzalez@codicesoftware.com>

--- src/cm/commands/getWorkspaceFromPath/getWorkspaceFromPath.ts
+++ src/cm/commands/getWorkspaceFromPath/getWorkspaceFromPath.ts
@@ -3,28 +3,26 @@ import { ICmResult, ICmShell } from "../../shell";
 import { CommandInfo } from "./commandInfo";
 import { GetWorkspaceFromPathParser } from "./getWorkspaceFromPathParser";
 
-export class GetWorkspaceFromPath {
-  public static async run(path: string, shell: ICmShell): Promise<IWorkspaceInfo | undefined> {
-    const parser = new GetWorkspaceFromPathParser();
+export async function run(path: string, shell: ICmShell): Promise<IWorkspaceInfo | undefined> {
+  const parser = new GetWorkspaceFromPathParser();
 
-    const cmResult: ICmResult<IWorkspaceInfo | undefined> =
-      await shell.exec(
-        CommandInfo.commandName,
-        [path,
-          '--format="'
-          + `{${CommandInfo.fields.guid.name}}` + CommandInfo.fieldSeparator
-          + `{${CommandInfo.fields.wkName.name}}` + CommandInfo.fieldSeparator
-          + `{${CommandInfo.fields.wkPath.name}}"`],
-        parser);
+  const cmResult: ICmResult<IWorkspaceInfo | undefined> =
+    await shell.exec(
+      CommandInfo.commandName,
+      [path,
+        '--format="'
+        + `{${CommandInfo.fields.guid.name}}` + CommandInfo.fieldSeparator
+        + `{${CommandInfo.fields.wkName.name}}` + CommandInfo.fieldSeparator
+        + `{${CommandInfo.fields.wkPath.name}}"`],
+      parser);
 
-    if (!cmResult.success) {
-      return undefined;
-    }
-
-    if (cmResult.error) {
-      throw cmResult.error;
-    }
-
-    return cmResult.result;
+  if (!cmResult.success) {
+    return undefined;
   }
+
+  if (cmResult.error) {
+    throw cmResult.error;
+  }
+
+  return cmResult.result;
 }
